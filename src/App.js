edc1519221449216fc7cb06f6df0c87773baf81a
ogasawara kakeru <kakeru19870423@gmail.com>
2022-07-10 11:15:46 +0900

    yarn add apllo-boost, graphql, react-apollo packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-console.log(GITHUB_TOKEN);
+const headersLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`,
+    },
+  });
+  return forward(operation);
+});
 
-function App() {
-  return <div>Hello, Graphql</div>;
-}
+const endpoint = "https://api.github.com/graphql";
+const httpLink = new HttpLink({ uri: endpoint });
+const link = ApolloLink.from([headersLink, httpLink]);
 
-export default App;
+export default new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
